cd85195982727216b36df06d605e447f4e3d6b2b
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-09-08 14:11:03 +0200

    Start creation of home page

---
+++ View/Components/Header/Home.php
@@ -0,0 +1,6 @@
+<header class="dashboard">
+    <section class="container">
+        <p><img src="View/Style/Assets/logo.png"></p>
+        <h1>Exercise<br>Looper</h1>
+    </section>
+</header>
--- View/Home.php
+++ View/Home.php
@@ -1,1 +1,26 @@
 <?php
+ob_start();
+include_once "Components/Header/Home.php"
+?>
+
+
+    <div class="container dashboard">
+        <section class="row">
+            <div class="column">
+                <a class="button answering column" href="/exercises/answering">Take an exercise</a>
+            </div>
+            <div class="column">
+                <a class="button managing column" href="/exercises/new">Create an exercise</a>
+            </div>
+            <div class="column">
+                <a class="button results column" href="/exercises">Manage an exercise</a>
+            </div>
+        </section>
+    </div>
+
+
+<?php
+
+$contenu = ob_get_clean(); // stocks la page dans la variable
+
+require "Layout.php";
