ea0ac9c1edbe04e9b3688f36a27eac0b60d6d224
TGR <78166079+TGACPNV@users.noreply.github.com>
2021-09-10 11:54:36 +0200

    add specific controllers

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -2,11 +2,50 @@
 
 class mainController
 {
-
+    private ExerciseController $exerciseController;
+    private FieldController $fieldController;
+    private AnswerController $answerController;
+    private TakeController $takeController;
 
     public function __construct()
     {
+        $this->exerciseController = new ExerciseController();
+        $this->fieldController = new FieldController();
+        $this->answerController = new AnswerController();
+        $this->takeController = new TakeController();
+    }
 
+    /
+    /**
+     * @return ExerciseController
+     */
+    public function getExerciseController(): ExerciseController
+    {
+        return $this->exerciseController;
+    }
+
+    /**
+     * @return FieldController
+     */
+    public function getFieldController(): FieldController
+    {
+        return $this->fieldController;
+    }
+
+    /**
+     * @return AnswerController
+     */
+    public function getAnswerController(): AnswerController
+    {
+        return $this->answerController;
+    }
+
+    /**
+     * @return TakeController
+     */
+    public function getTakeController(): TakeController
+    {
+        return $this->takeController;
     }
 
 
