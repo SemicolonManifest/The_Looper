a3c84882e2c64e1231cd976756d04f9524c55b7e
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-09-10 10:36:56 +0200

    Create header to answering section

---
+++ View/Components/Header/Answering.php
@@ -0,0 +1,10 @@
+<?php
+ob_start();
+?>
+    <header class="take">
+            <a><img id="Logo" src="View/Style/Assets/logo.png"></a>
+            <h1>Exercise<br>Looper</h1>
+    </header>
+
+<?php
+$header = ob_get_clean();
