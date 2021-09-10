9df29b8d1b60dd779e5798136aff639301122bed
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-09-10 11:34:08 +0200

    Create all headers

---
+++ View/Components/Header/Managing.php
@@ -0,0 +1,9 @@
+<?php
+ob_start();
+?>
+    <header class="managing">
+        <a><img id="Logo" src="View/Style/Assets/logo.png"></a>
+    </header>
+
+<?php
+$header = ob_get_clean();
---
+++ View/Components/Header/Results.php
@@ -0,0 +1,9 @@
+<?php
+ob_start();
+?>
+    <header class="results">
+        <a><img id="Logo" src="View/Style/Assets/logo.png"></a>
+    </header>
+
+<?php
+$header = ob_get_clean();
