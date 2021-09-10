78a1c54d8d281e4ae276c01e94d7e68e5bf49130
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-09-10 12:06:25 +0200

    Create page to create exercise

--- Index.php
+++ Index.php
@@ -1,3 +1,3 @@
 <?php
 
-require_once "View/TakeExercise.php";
+require_once "View/CreateExercise.php";
--- View/CreateExercise.php
+++ View/CreateExercise.php
@@ -2,10 +2,12 @@
 ob_start();
 ?>
     <div class="container">
+        <h1 class="text">New Exercise</h1>
         <section class="row">
             <div class="column">
-                <div class="embed">
-                    <p class="text">name exercise</p>
+                <div>
+                    <label for="exercise_title">Title</label>
+                    <input id="exercise_title" type="text" name="exercise[title]">
                     <div class="button-container"><a class="answering button" href="/exercises/IDExercise/fulfillments/new">TAKE IT</a></div>
                 </div>
             </div>
@@ -14,7 +16,7 @@ ob_start();
 
 
 <?php
-$headerPath = "Components/Header/Answering.php";
+$headerPath = "Components/Header/Managing.php";
 $contenu = ob_get_clean();
 
 require "Layout.php";
