06f0e4e24ec837cb9fdfe8adf4ac243094b948f4
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-09-10 11:19:38 +0200

    Create TakeExercise

---
+++ View/TakeExercise.php
@@ -0,0 +1,20 @@
+<?php
+ob_start();
+?>
+    <div class="container dashboard">
+        <section class="row">
+            <div class="column">
+                <div class="embed">
+                    <p class="text">name exercise</p>
+                    <div class="button-container"><a class="answering button">TAKE IT</a></div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+
+<?php
+$headerPath = "Components/Header/Answering.php";
+$contenu = ob_get_clean();
+
+require "Layout.php";
