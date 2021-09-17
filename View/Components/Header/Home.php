a848a1c3523a30af5d1dac7201c3df56c2bbfb99
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-09-17 12:05:45 +0200

    Add link to return in home page

--- View/Components/Header/Home.php
+++ View/Components/Header/Home.php
@@ -3,7 +3,7 @@ ob_start();
 ?>
     <header class="dashboard">
         <section class="container">
-            <a><img id="LogoDashboard" src="View/Style/Assets/logo.png"></a>
+            <a href="?"><img id="LogoDashboard" src="View/Style/Assets/logo.png"></a>
             <h1>Exercise<br>Looper</h1>
         </section>
     </header>
