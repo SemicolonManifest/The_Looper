edd3b9556a8f1498e082de082800b14da8361131
TGR <78166079+TGACPNV@users.noreply.github.com>
2021-09-17 11:30:33 +0200

    add index action if action not set

--- Index.php
+++ Index.php
@@ -5,10 +5,16 @@ date_default_timezone_set('Europe/Zurich');
 require_once 'Controller/MainController.php';
 $mainController = new mainController();
 
-switch ($_GET['action']){
-    case 'createExercise':
-        $mainController->createExercise();
-        break;
-    default:
-        $mainController->showHome();
+if (isset($_GET['action'])) {
+
+    switch ($_GET['action']) {
+        case 'createExercise':
+            $mainController->createExercise();
+            break;
+        default:
+            $mainController->showHome();
+    }
+
+}else{
+    $mainController->showHome();
 }
