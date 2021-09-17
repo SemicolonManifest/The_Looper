c21e257fc0d269671abad18ba38c4b6b8b3701d2
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-09-17 12:06:06 +0200

    Add link to go on different page

--- View/Home.php
+++ View/Home.php
@@ -3,13 +3,13 @@ ob_start();
 ?>
 
     <section class="row">
-            <a class="button answering column" href="/exercises/answering">Take an exercise</a>
+            <a class="button answering column" href="?action=showAllExercises">Take an exercise</a>
 
 
-            <a class="button managing column" href="/exercises/new">Create an exercise</a>
+            <a class="button managing column" href="?action=showCreateExercise">Create an exercise</a>
 
 
-            <a class="button results column" href="/exercises">Manage an exercise</a>
+            <a class="button results column" href="?action=showManageExercise">Manage an exercise</a>
 
     </section>
 
