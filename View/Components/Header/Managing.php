87395b72e74618aa9290f88252036e81b0f6baba
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-10-08 10:48:46 +0200

    Add name of exercise when an exercise is created

--- View/Components/Header/Managing.php
+++ View/Components/Header/Managing.php
@@ -4,6 +4,9 @@ ob_start();
     <header class="managing">
         <section class="container">
             <a href="?"><img id="Logo" src="View/Style/Assets/logo.png"></a>
+            <?php if (isset($_POST['exercise']['title'])) { ?>
+                <a>Exercise : <span><?= $_POST['exercise']['title'] ?></span></a>
+            <?php } ?>
         </section>
     </header>
 
