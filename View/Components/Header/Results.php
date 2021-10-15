e69205979eac17bea341f27d368bffb9c5ed7c85
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-10-15 11:29:08 +0200

    Add title in result header

--- View/Components/Header/Results.php
+++ View/Components/Header/Results.php
@@ -4,7 +4,7 @@ ob_start();
     <header class="results">
         <section class="container">
             <a href="?"><img id="Logo" src="View/Style/Assets/logo.png"></a>
-            <?php if ($_GET['action'] == "showStatExercise") { ?>
+            <?php if ($_GET['action'] == "showStatExercise" || $_GET['action'] == "showStatExerciseByField" || $_GET['action'] == "showStatExerciseByTake"  ) { ?>
                 <a>Exercise : <span><?= $exercise->title ?></span></a>
             <?php } ?>
         </section>
