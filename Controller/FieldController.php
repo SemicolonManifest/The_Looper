f9f0ea8ec54709934b5c7817b408521c910a23b8
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-10-15 13:51:29 +0200

    Add method find in TakeController

--- Controller/FieldController.php
+++ Controller/FieldController.php
@@ -5,12 +5,9 @@ use TheLooper\Model\Field;
 class FieldController
 {
     public function __construct(){
-
     }
-
 
     public function find(int $id){
         return Field::find($id);
-
     }
 }
