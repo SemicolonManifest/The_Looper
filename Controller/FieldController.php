109b68948570093c6cde455e849c6e569e968d1e
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-10-15 11:28:20 +0200

    Add method find in field controller

--- Controller/FieldController.php
+++ Controller/FieldController.php
@@ -1,6 +1,16 @@
 <?php
 namespace TheLooper\Controller;
+use TheLooper\Model\Field;
+
 class FieldController
 {
+    public function __construct(){
 
+    }
+
+
+    public function find(int $id){
+        return Field::find($id);
+
+    }
 }
