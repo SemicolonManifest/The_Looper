0675aa953f3aa715a63ba29739ed04e857fcb7c7
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-11-15 10:23:47 +0100

    Add filter to only see exercises on answering

--- View/TakeExercise.php
+++ View/TakeExercise.php
@@ -1,14 +1,15 @@
-
-        <section class="row">
-            <div class="column">
-                <?php foreach ($exercises as $exercise ){ ?>
+<section class="row">
+    <div class="column">
+        <?php foreach ($exercises as $exercise): ?>
+            <?php if ($exercise['state'] == \TheLooper\Model\ExerciseState::ANSWERING): ?>
                 <div class="embed">
                     <p class="text"><?= $exercise['title'] ?></p>
                     <div class="button-container">
                         <a class="answering button" href="?action=showExercise&id=<?= $exercise['id'] ?>">TAKE IT</a>
                     </div>
                 </div>
-                <?php } ?>
-            </div>
-        </section>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </div>
+</section>
 
