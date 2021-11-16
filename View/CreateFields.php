3295056bde0107ca0f2cadbb580f0ad781795a11
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-11-16 14:09:55 +0100

    Display list of field by exercise

--- View/CreateFields.php
+++ View/CreateFields.php
@@ -1,45 +1,58 @@
+<div class="row">
+    <div class="column">
+        <h1 class="text">Fields</h1>
+        <table class="records table">
+            <thead>
+            <tr>
+                <th>Label</th>
+                <th>Value kind</th>
+                <th> </th>
+            </tr>
+            </thead>
 
-    <div class="row">
-        <div class="column">
-            <h1 class="text">Fields</h1>
-            <table class="records table">
-                <thead>
+            <tbody>
+            <?php foreach ($exercise->fields() as $field): ?>
                 <tr>
-                    <th>Label</th>
-                    <th>Value kind</th>
-                    <th></th>
+                    <td>
+                        <?= $field->label ?>
+                    </td>
+                    <td>
+                        <?= ($field->value_kind != \TheLooper\Model\FieldValueKind::SINGLE_LINE) ? ($field->value_kind != \TheLooper\Model\FieldValueKind::LIST_OF_LINES) ? ($field->value_kind == \TheLooper\Model\FieldValueKind::MULTI_LINES) ? "multi_lines" : "" : "list_of_lines" : "single_line" ?>
+                    </td>
+                    <td id="iconColumn">
+                        <a class="icon" href="?action=showEditField&id=<?= $field->getId() ?>"><i class="fa fa-edit"></i></a>
+                        <a class="icon" href="?action=deleteField&id=<?= $field->getId() ?>"><i class="fa fa-trash"></i></a>
+                    </td>
                 </tr>
-                </thead>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
 
-                <tbody>
-                </tbody>
-            </table>
-
-            <a class="button answering"
-               data-method="put" href="/exercises/441?exercise%5Bstatus%5D=answering"><i
-                        class="fa fa-comment"></i> Complete and be ready for answers</a>
-        </div>
-        <div class="column">
-            <h1 class="text">New Fields</h1>
-            <form action="" method="get">
-                <input type="hidden" name="action" value="createField">
-                <input type="hidden" name="exercise_id" value="<?= $exercise->id ?>">
-                <div class="field">
-                    <label for="field_label">Label</label>
-                    <input class="input" id="field_label" type="text" name="field[label]">
-                    <label for="field_value">Value kind</label>
-                    <select class="input" id="field_value" name="field[value]">
-                        <option value="0">Single line text</option>
-                        <option value="1">List of single lines</option>
-                        <option value="2">Multi-line text</option>
-                    </select>
-                </div>
-                <div class="actions">
-                    <input type="submit" name="commit" value="Create Field" class="answering button">
-                </div>
-            </form>
-
-        </div>
-
+        <a class="button answering"
+           data-method="put" href="/exercises/441?exercise%5Bstatus%5D=answering"><i
+                    class="fa fa-comment"></i> Complete and be ready for answers</a>
+    </div>
+    <div class="column">
+        <h1 class="text">New Fields</h1>
+        <form action="" method="get">
+            <input type="hidden" name="action" value="createField">
+            <input type="hidden" name="exercise_id" value="<?= $exercise->id ?>">
+            <div class="field">
+                <label for="field_label">Label</label>
+                <input class="input" id="field_label" type="text" name="field[label]">
+                <label for="field_value">Value kind</label>
+                <select class="input" id="field_value" name="field[value]">
+                    <option value="0">Single line text</option>
+                    <option value="1">List of single lines</option>
+                    <option value="2">Multi-line text</option>
+                </select>
+            </div>
+            <div class="actions">
+                <input type="submit" name="commit" value="Create Field" class="answering button">
+            </div>
+        </form>
 
     </div>
+
+
+</div>
