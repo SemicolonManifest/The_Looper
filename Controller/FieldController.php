eb6eba484e66481cdf9a8fa9a0cbcfc952804e9a
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-11-16 13:49:35 +0100

    Add method createField and update method showCreateField

--- Controller/FieldController.php
+++ Controller/FieldController.php
@@ -1,6 +1,7 @@
 <?php
 namespace TheLooper\Controller;
 use TheLooper\Model\Field;
+use TheLooper\Model\Exercise;
 
 class FieldController
 {
@@ -10,11 +11,36 @@ class FieldController
     public static function showCreateField()
     {
         ob_start();
-        ExerciseController::create($_POST['exercise']['title']);
+        $exercise = new Exercise();
+
+        if(isset($_POST['exercise']['title'])){
+            $exercise->title = $_POST['exercise']['title'];
+            $exercise->create();
+        }
+        else{
+            $exercise = Exercise::find($_GET['id']);
+        }
+
+
         include_once "View/CreateFields.php";
         $headerPath = "Components/Header/Managing.php";
         $contenu = ob_get_clean();
 
         require dirname(__DIR__, 1) . "/View/Layout.php";
+    }
+
+    public static function createField(){
+        ob_start();
+        $exercise = Exercise::find($_GET['exercise_id']);
+        $field = new Field($_GET["field"]["label"], $_GET["field"]["value"], $exercise->id);
+        $field->create();
+
+
+        include_once "View/CreateFields.php";
+        $headerPath = "Components/Header/Managing.php";
+        $contenu = ob_get_clean();
+
+        require dirname(__DIR__, 1) . "/View/Layout.php";
+
     }
 }
