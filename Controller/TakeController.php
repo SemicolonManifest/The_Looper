8e6fc4e7b84cbf901a929d50459febb9625eae5e
TGACPNV <78166079+TGACPNV@users.noreply.github.com>
2021-12-12 16:25:18 +0100

    code Take's edit function

--- Controller/TakeController.php
+++ Controller/TakeController.php
@@ -2,6 +2,7 @@
 
 namespace TheLooper\Controller;
 
+use TheLooper\Model\Answer;
 use TheLooper\Model\Exercise;
 use TheLooper\Model\Take;
 
@@ -32,5 +33,25 @@ class TakeController
 
         require dirname(__DIR__, 1) . "/View/Layout.php";
 
+    }
+
+    static function edit()
+    {
+        if (isset($_GET["take"]) && isset($_GET['answers']))
+        {
+            $takeId = $_GET["take"];
+            $_SESSION['saveSuccess'] = true;
+
+            foreach ($_GET['answers'] as $id => $answerValue){
+                $answer = Answer::find($id);
+                $answer->response = $answerValue;
+                if(!$answer->save()) $_SESSION['saveSuccess'] = false;
+            }
+
+        }else{
+            $_SESSION['saveSuccess'] = false;
+        }
+
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
     }
 }
