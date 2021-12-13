bfe37a08a405490fb3f1657179d9c97c7ca69458
TGR <78166079+TGACPNV@users.noreply.github.com>
2021-12-13 11:24:58 +0100

    remove not used attribute

--- Model/Exercise.php
+++ Model/Exercise.php
@@ -9,7 +9,6 @@ class Exercise
     public int $id;
     public string $title;
     public int $state = 0;
-    public array $fields;
 
     public function __construct(string $title = "", int $id = -1, int $state = -1, array $fields = [])
     {
