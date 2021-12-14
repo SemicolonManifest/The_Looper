bdb8a1d12dcf16278d5f1c82103c69827e7d012b
TGACPNV <78166079+TGACPNV@users.noreply.github.com>
2021-12-14 17:27:30 +0100

    adding some comments

--- Model/Answer.php
+++ Model/Answer.php
@@ -7,11 +7,11 @@ class Answer
     public Field $field;
     public Take  $take;
 
-    /*public function __construct(string $response,array $take){
-        $this->response = $response;
-        $this->take = $take;
-    }*/
-
+    /**
+     * @description This function insert the answer in the database and set the id attribute
+     * @return bool - Success or fail
+     *
+     */
     public function create(): bool
     {
         $this->id = DBConnector::insert("INSERT INTO answers (response, fields_id, takes_id) values (:response, :fields_id, :takes_id);", ['response' => $this->response, 'fields_id' => $this->field->getId(), 'takes_id' => $this->take->id]);
@@ -19,6 +19,10 @@ class Answer
         return true;
     }
 
+    /** @description This function return an object of Answer
+     * @param array $params - The attributes of the class
+     * @return Answer
+     */
     static function make(array $params)
     {
         $answer = new Answer();
@@ -42,6 +46,9 @@ class Answer
         return $answer;
     }
 
+    /** @description This method returns an array of all the Answer
+     * @return array - The array of Answer
+     */
     static function all(): array
     {
         $result = DBConnector::selectmany("SELECT id, response, fields_id, takes_id FROM answers;", []);
@@ -52,6 +59,10 @@ class Answer
         return $return;
     }
 
+    /** @description This method is used to get a specific Answer with it's ID
+     * @param int $id
+     * @return Answer|null - Will return null of none found
+     */
     static function find(int $id): ?Answer
     {
         $res = DbConnector::selectOne("SELECT * FROM answers WHERE id = :id", ['id' => $id]);
@@ -64,6 +75,11 @@ class Answer
 
     }
 
+    /** @description This method return all Answer where a field as a said value
+     * @param $field - The database column
+     * @param $value - The value
+     * @return array - Array of Answer
+     */
     static function where($field,$value): array
     {
         $result = DbConnector::selectMany("select * from answers where $field = :value;",["value"=>$value]);
@@ -74,16 +90,26 @@ class Answer
         return $return;
     }
 
+    /** @description Mathod to save the Answer
+     * @return bool - Success or fail
+     */
     public function save(): bool
     {
         return DbConnector::execute("UPDATE answers set response = :response WHERE id = :id", ['id' => $this->id, 'response' => $this->response]);
     }
 
+    /** @description Method to delete the Answer
+     * @return bool - Success or fail
+     */
     public function delete(): bool
     {
         return self::destroy($this->id);
     }
 
+    /** @description Method to destroy an answer
+     * @param int $id - The answer's ID
+     * @return bool - Success or fail
+     */
     static function destroy(int $id): bool
     {
         try {
--- Model/Take.php
+++ Model/Take.php
@@ -8,12 +8,10 @@ class Take
     public int $id;
     public DateTime $timeStamp;
 
-    /*public function __construct($timeStamp)
-    {
-        $this->timeStamp = $timeStamp;
-    }*/
-
-
+    /** @description This function will insert the take in the database and set the id attribute
+     * @return bool
+     * @throws \Exception
+     */
     public function create(): bool
     {
         $this->id = DBConnector::insert("INSERT INTO takes values ();");
@@ -21,6 +19,10 @@ class Take
         return true;
     }
 
+    /** @description This function return an object of Take
+     * @param array $params - The attributes of the class
+     * @return Take
+     */
     static function make(array $params)
     {
         $take = new Take();
@@ -34,6 +36,9 @@ class Take
         return $take;
     }
 
+    /** @description This method returns an array of all the Takes
+     * @return array
+     */
     static function all(): array
     {
         $result = DBConnector::selectmany("SELECT id, time_stamp FROM takes;", []);
@@ -46,6 +51,10 @@ class Take
 
     }
 
+    /** @description This method is used to get a specific Take with it's ID
+     * @param int $id
+     * @return Take|null
+     */
     static function find(int $id): ?Take
     {
         $res = DbConnector::selectOne("SELECT * FROM takes WHERE id = :id", ['id' => $id]);
@@ -58,6 +67,11 @@ class Take
 
     }
 
+    /** @description This method return all Takes where a field as a said value
+     * @param $field - The database column
+     * @param $value - The value
+     * @return array
+     */
     static function where($field,$value): array
     {
         $result = DbConnector::selectMany("select * from takes where $field = :value;",["value"=>$value]);
@@ -68,11 +82,18 @@ class Take
         return $return;
     }
 
+    /** @description Method to delete the Take
+     * @return bool
+     */
     public function delete(): bool
     {
         return self::destroy($this->id);
     }
 
+    /** @description Method to destroy a take
+     * @param int $id - It's ID
+     * @return bool - Success or fail
+     */
     static function destroy(int $id): bool
     {
         try {
@@ -83,6 +104,9 @@ class Take
         }
     }
 
+    /** @description Method to get all answers of a Take
+     * @return array
+     */
     public function answers(): array
     {
         $answers = Answer::where("takes_id",$this->id);
